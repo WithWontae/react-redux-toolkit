f889f13ab6d5a52065853e33563c11cfb8733d7d
WonTae Kim(김원태 - Mobile Dev. Division) <wontae@qoo10.com>
2025-01-07 23:03:24 +0900

    reduxt-toolkit 사용

--- src/App.js
+++ src/App.js
@@ -1,27 +1,56 @@
 import React from 'react';
 import { createStore } from 'redux';
 import { Provider, useSelector, useDispatch } from 'react-redux';
+import { configureStore, createSlice } from '@reduxjs/toolkit';
 
-function reducer(state, action) {
-  if (action.type === 'up') {
-    return {...state, value: state.value + action.step};
+const counterSlice = createSlice({
+  name: 'counterSlice',
+  initialState: {value: 0},
+  reducers: {
+    up: (state, action) => {
+      console.log(action);
+      state.value += action.step;
+    }
   }
+});
 
-  return state;
-}
+const addSlice = createSlice({
+  name: 'addSlice',
+  initialState: {value: 0},
+  reducers: {
+    add: (state, action) => {
+      state.value += action.step;
+    }
+  }
+});
 
-const initialState = {value: 0}
-const store = createStore(reducer, initialState);
+const store = configureStore({
+  reducer: {
+    counter: counterSlice.reducer,
+    add: addSlice.reducer
+  }
+});
+
+// function reducer(state, action) {
+//   if (action.type === 'up') {
+//     return {...state, value: state.value + action.step};
+//   }
+
+//   return state;
+// }
+
+// const initialState = {value: 0}
+// const store = createStore(reducer, initialState);
 
 function Counter() {
   const dispatch = useDispatch();
-  const counter = useSelector(state => state.value);
+  const count = useSelector(state => state.counter.value);
 
   return (
     <div>
       <button onClick={
-        () => dispatch({type: 'up', step: 2})
-      }>+</button> {counter}
+        () => dispatch({type:'counterSlice/up', step:2})
+      }>+</button> {count}
     </div>
   );
 }
